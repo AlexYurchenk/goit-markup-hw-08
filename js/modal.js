5f85290cf590778281c10dad5c82b6188ab1f641
AlexYurchenk <75500247+AlexYurchenk@users.noreply.github.com>
2021-02-14 22:09:14 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -13,3 +13,11 @@
     refs.modal.classList.toggle('backdrop--hidden');
   }
 })();
+window.onresize = function(){
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+    if(document.documentElement.clientWidth>767 && menuBtnRef.classList.contains('is-open')){
+        mobileMenuRef.classList.toggle("is-open");
+        menuBtnRef.classList.toggle("is-open");
+    }
+}
